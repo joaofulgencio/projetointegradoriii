7427125419a870937a0cb614f357a5050de93800
joaofulgenciozup <joao.fulgencio@zup.com.br>
2024-05-09 11:49:07 -0300

    Adicionando LOGOUT

--- app/src/main/java/com/joao/fulgencio/fragmentos/MainActivity.kt
+++ app/src/main/java/com/joao/fulgencio/fragmentos/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.joao.fulgencio.fragmentos
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
 import androidx.navigation.fragment.NavHostFragment
@@ -10,17 +14,22 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.joao.fulgencio.fragmentos.databinding.ActivityMainBinding
 
+private const val SESSION_PREF = "session_pref"
+private const val LOGGED_IN = "logged_in"
+private const val LOGIN_TIME = "login_time"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+        sharedPreferences = getSharedPreferences(SESSION_PREF, Context.MODE_PRIVATE)
         initToolbar()
     }
 
@@ -36,4 +45,38 @@ class MainActivity : AppCompatActivity() {
         Log.d("NavController", "Navigate up: $navigateUp")
         return navigateUp || super.onSupportNavigateUp()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu) // Infla o arquivo menu_main.xml
+        return true
+    }
+
+    // Gerencia as ações de clique nos itens do menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                logout()
+                Log.d("MainActivity", "Logout action clicked")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun logout() {
+        // Limpa os dados do SharedPreferences
+        with(sharedPreferences.edit()) {
+            remove(LOGGED_IN)
+            remove(LOGIN_TIME)
+            apply()
+        }
+
+        // Volta ao fragmento de login (FirstFragment)
+        val navHostFragment = binding.navHostFragment.getFragment<NavHostFragment>()
+        navController = navHostFragment.navController
+        navController.navigate(R.id.action_global_firstFragment)
+
+        Log.d("MainActivity", "Logout action completed")
+    }
+
 }
